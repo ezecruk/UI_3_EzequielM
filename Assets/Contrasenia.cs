445e405248da288df871c5503d8a0e3dbb87d776
ezecruk <164010116+ezecruk@users.noreply.github.com>
2024-05-28 09:07:55 -0300

    Ui_contraseña_12345

---
+++ Assets/Contrasenia.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Contrasenia : MonoBehaviour
+{
+    public string contraseña;
+    public InputField impContra;
+    public void contra()
+    {
+        if (impContra.text == contraseña)
+        {
+            Debug.Log("¿Lo habéis conseguido? ¿1-2-3-4-5? ¡Es la misma contraseña que tengo en mis maletas!");
+        }
+        else
+        {
+            Debug.Log("¿¡¿1-2-3-4-5?!? ¡Ésa es la contraseña que un idiota pondría en sus maletas!");
+        }
+    }
+}
